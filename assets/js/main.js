d9fd09c2f96fbceefa84d3b21d881f36beeaaf7d
Rushi Chaudhari <rushic24@gmail.com>
2019-08-16 19:31:16 +0530

    updated cards

--- assets/js/main.js
+++ assets/js/main.js
@@ -88,16 +88,7 @@ Copyright: Akshay Goel
 
 })(jQuery);
 
-function topBarData() {
-	return '<ul class="navList">'+
-	'<li><a href="../index.html" id="to_home">Home</a></li>'+
-	'<li><a href="../index.html#introduction_tag" id="to_intro">Introduction</a></li>'+
-	'<li><a href="#work_tag" id="to_work">Work</a></li>' +
-	'<li><a href="../index.html#projects_tag" id="to_projects">Projects</a></li>'+
-	'<li><a href="../index.html#contactme_tag" id="to_contact">Contact Me</a></li>'+
-	'<li><a href="../documents/AkshayGoel_Resume.pdf" target="_blank">Resume</a></li>'+
-'</ul>';
-}
+
 
 function loadTopNavBar() {
  	var path = window.location.pathname;
@@ -160,13 +151,3 @@ var $root = $('html, body');
 		return false;
 	});
 
- /*
- 	Google Analytics
- */
- (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-    ga('create', 'UA-79770649-1', 'auto');
-    ga('send', 'pageview');
