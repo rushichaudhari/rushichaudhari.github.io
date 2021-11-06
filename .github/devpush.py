1b071200eb0e1bebaf44f4305161d531e0fcd240
rushichaudhari <rushic42@gmail.com>
2021-11-06 12:24:36 -0400

    fix json

--- .github/devpush.py
+++ .github/devpush.py
@@ -99,14 +99,14 @@ if __name__ == "__main__":
                 json=json.loads(data),
                 headers={"api_key": os.environ["DEVTO_TOKEN"]},
                 )
-                print(result)
+                print(result.json())
             else:
                 result = requests.post(
                     url=URL,
                     json=json.loads(data),
                     headers={"api_key": os.environ["DEVTO_TOKEN"]},
                 )
-                print(result)
+                print(result.json())
             print(file)
         else:
             print(file, "no change")
